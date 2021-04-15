2a5ecff15a6fa14a2ed89cc80b6c84fa8d26922b
Ayushi gaurav <53953293+Ayushi76@users.noreply.github.com>
2021-04-15 14:19:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
